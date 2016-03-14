0001b4b3e498c2b1b4eb08129c45b34bf26080d1
makg <makg@vet.dtu.dk>
2016-03-14 15:21:10 +0100

    added example for including external R-code

--- case1.R
+++ case1.R
@@ -1,1 +1,6 @@
 ## ---- chunkname
+# you have to start a chunk with ## ----
+# and then add the name of the chunk, if you want to have it in another file
+# then the file has to be read by read_chunk in the beginning
+1 + 1
+plot(seq(5),seq(5))
